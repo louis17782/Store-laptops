f623ff418be3b3a3621b0c3d76f7fff9f7d723f2
louis17782 <luissanteliz22@gmail.com>
2026-08-17 16:41:49 -0400

    fix styles

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -2,7 +2,7 @@ import WhatsAppButton from "./WhatsAppButton";
 
 export default function Cart({ cart }) {
   return (
-    <div>
+    <div className="content-cart">
       <h2>Carrito</h2>
       {cart.length === 0 ? (
         <p>No hay productos en el carrito.</p>
